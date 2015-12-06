7cf25593294a36f9faa9674d1abf364b542ff565
Dmitry <ezikvice@mail.ru>
2015-12-06 14:38:14 +0300

    рефакторинг: позиция объекта в поле теперь задается туплом добавлены цели для коробок

--- GameObjects.py
+++ GameObjects.py
@@ -6,46 +6,40 @@ __author__ = 'Dmitry'
 
 
 class GameObject(sprite.Sprite):
-
-    def __init__(self, img, obj_id, row, column, batch):
+    def __init__(self, img, obj_id, batch, arr):
+        row, column = arr
         super(GameObject, self).__init__(img, column * CELL_SIZE, WIN_HEIGHT - row * CELL_SIZE, batch=batch)
         self.column = column
         self.row = row
         self.obj_id = obj_id
 
-    def move(self, row, column):
+    def move(self, arr):
+        row, column = arr
         self.column = column
         self.row = row
         self.set_position(column * CELL_SIZE, WIN_HEIGHT - row * CELL_SIZE)
 
 
 class Player(GameObject):
-
-    def __init__(self, column, row, batch):
-        super(Player, self).__init__(res.player, 1, column, row, batch)
+    def __init__(self, batch, arr):
+        super(Player, self).__init__(res.player, 1, batch, arr)
 
 
 class Tree(GameObject):
-
-    def __init__(self, column, row, batch):
-        super(Tree, self).__init__(res.pinetree, 2, column, row, batch)
+    def __init__(self, batch, arr):
+        super(Tree, self).__init__(res.pinetree, 2, batch, arr)
 
 
 class Brick(GameObject):
-
-    def __init__(self, column, row, batch):
-        super(Brick, self).__init__(res.brick, 3, column, row, batch)
+    def __init__(self, batch, arr):
+        super(Brick, self).__init__(res.brick, 3, batch, arr)
 
 
 class Box(GameObject):
-
-    def __init__(self, column, row, batch):
-        super(Box, self).__init__(res.box, 4, column, row, batch)
+    def __init__(self, batch, arr):
+        super(Box, self).__init__(res.box, 4, batch, arr)
 
 
 class BoxTarget(GameObject):
-
-    def __init__(self, column, row, batch):
-        super(Brick, self).__init__(res.brick, 10, column, row, batch)
-
-
+    def __init__(self, batch, arr):
+        super(BoxTarget, self).__init__(res.target, 10, batch, arr)
--- pygletTest.py
+++ pygletTest.py
@@ -5,7 +5,6 @@ import numpy as np
 import GameObjects
 from GameMetric import *
 
-
 __author__ = 'Dmitry'
 
 # TODO: сделать нормальный расчет координат. сейчас неправильно считаются
@@ -13,15 +12,15 @@ __author__ = 'Dmitry'
 # shape = (9, 9)
 # arr = np.ones((9, 9))
 # TODO: загружать поле из файла
-arr = np.array([[2, 2, 2, 2, 2, 2, 2, 2, 2],
-               [2, 3, 3, 3, 3, 3, 3, 3, 2],
-               [2, 3, 1, 0, 0, 0, 0, 3, 2],
-               [2, 3, 0, 0, 0, 0, 0, 3, 2],
-               [2, 3, 0, 0, 0, 0, 0, 3, 2],
-               [2, 3, 0, 4, 0, 0, 0, 3, 2],
-               [2, 3, 0, 0, 0, 0, 0, 3, 2],
-               [2, 2, 3, 3, 3, 3, 3, 2, 2],
-               [0, 2, 2, 2, 2, 2, 2, 2, 0]])
+arr = np.array([[3, 3, 3, 3, 3, 3, 3, 3, 2],
+                [3, 0, 0, 0, 0, 0, 0, 3, 2],
+                [3, 0, 1, 0, 0, 0, 0, 3, 2],
+                [3, 0, 0, 0, 0, 10, 0, 3, 2],
+                [3, 0, 0, 0, 0, 0, 0, 3, 2],
+                [3, 0, 0, 14, 0, 0, 0, 3, 2],
+                [3, 0, 0, 0, 0, 0, 0, 3, 2],
+                [2, 3, 3, 3, 3, 3, 3, 2, 2],
+                [0, 2, 2, 2, 2, 2, 2, 2, 0]])
 
 pyglet.resource.path = ["res"]
 pyglet.resource.reindex()
@@ -30,28 +29,37 @@ pyglet.resource.reindex()
 # player = pyglet.sprite.Sprite(res.player, x=50, y=50)
 
 batch = pyglet.graphics.Batch()
-# player = GameObjects.Player()
+layer2 = pyglet.graphics.Batch()
+
 trees = []  # TODO: сделать словарик с ресурсами (номер-имя), или лучше спрайты сделать объектами и в них хранить также номер
 bricks = []
 boxes = []
-current_cell = [0, 0]
-winwidth = CELL_SIZE*10
-winheight = CELL_SIZE*10
+box_targets = []
+
+current_cell = 0, 0
+
+player = GameObjects.Player(None, current_cell)
+
 arrsize = arr.size
+
 for row in range(len(arr)):
     for column in range(len(arr[row])):
-        current_cell = arr[row][column]
-        if current_cell == 2:
-            trees.append(GameObjects.Tree(row, column, batch))
-        if arr[row, column] == 3:
-            bricks.append(GameObjects.Brick(row, column, batch))
-        if arr[row, column] == 4:
-            boxes.append(GameObjects.Box(row, column, batch))
-        elif arr[row, column] == 1:
-            player = GameObjects.Player(row, column, None)
+        current_cell = row, column
+        cur_cell_val = arr[current_cell]
+        if cur_cell_val >= 10:
+            box_targets.append(GameObjects.BoxTarget(layer2, current_cell))  # кидаем во второй слой
+            cur_cell_val -= 10
+        if cur_cell_val == 2:
+            trees.append(GameObjects.Tree(batch, current_cell))
+        if cur_cell_val == 3:
+            bricks.append(GameObjects.Brick(batch, current_cell))
+        if cur_cell_val == 4:
+            boxes.append(GameObjects.Box(batch, current_cell))
+        elif arr[current_cell] == 1:
+            # player = GameObjects.Player(row, column, None)
+            player.move(current_cell)
 
-
-window = pyglet.window.Window(width=(CELL_SIZE*10), height=(CELL_SIZE*10), caption="Gecko Soko")
+window = pyglet.window.Window(width=(CELL_SIZE * 10), height=(CELL_SIZE * 10), caption="Gecko Soko")
 fps_display = pyglet.window.FPSDisplay(window)
 fps_display.label.y = 0
 
@@ -65,25 +73,30 @@ label = pyglet.text.Label('[{0}, {1}]'.format(player.row, player.column),
 def show_coords():
     label.text = '[{0}, {1}]'.format(player.row, player.column)
 
+
 @window.event
 def on_key_press(symbol, modifiers):
     c = player.column
     r = player.row
     if symbol == key.UP:  # координаты по y обращены для удобства
-        if arr[r-1][c] != 3:  # TODO: сделать проверку возможности хода (кирпич, ящик, а за ним что-нибудь)
-            player.move(r-1, c)
+        next_pos = [player.row - 1, player.column]
+        if arr[r - 1][c] != 3:  # TODO: сделать проверку возможности хода (кирпич, ящик, а за ним что-нибудь)
+            player.move(next_pos)
             show_coords()
     if symbol == key.DOWN:  # координаты по y обращены для удобства
-        if arr[r+1][c] != 3:  # TODO: сделать проверку возможности хода (кирпич, ящик, а за ним что-нибудь)
-            player.move(r+1, c)
+        next_pos = [player.row + 1, player.column]
+        if arr[r + 1][c] != 3:  # TODO: сделать проверку возможности хода (кирпич, ящик, а за ним что-нибудь)
+            player.move(next_pos)
             show_coords()
     if symbol == key.LEFT:
-        if arr[r][c-1] != 3:  # TODO: сделать проверку возможности хода (кирпич, ящик, а за ним что-нибудь)
-            player.move(r, c-1)
+        next_pos = [player.row, player.column - 1]
+        if arr[r][c - 1] != 3:  # TODO: сделать проверку возможности хода (кирпич, ящик, а за ним что-нибудь)
+            player.move(next_pos)
             show_coords()
     if symbol == key.RIGHT:
-        if arr[r][c+1] != 3:  # TODO: сделать проверку возможности хода (кирпич, ящик, а за ним что-нибудь)
-            player.move(r, c+1)
+        next_pos = [player.row, player.column + 1]
+        if arr[r][c + 1] != 3:  # TODO: сделать проверку возможности хода (кирпич, ящик, а за ним что-нибудь)
+            player.move(next_pos)
             show_coords()
 
 
@@ -91,6 +104,7 @@ def on_key_press(symbol, modifiers):
 def on_draw():
     window.clear()
     batch.draw()
+    layer2.draw()
     player.draw()
     label.draw()
     fps_display.draw()
@@ -99,7 +113,8 @@ def on_draw():
 def update(dt):
     pass
 
+
 pyglet.clock.set_fps_limit(60)
-pyglet.clock.schedule_interval(update, 1/120)
+pyglet.clock.schedule_interval(update, 1 / 120)
 
 pyglet.app.run()
